53c378fb3b876131d1988ebc1bd53dea5757b346
Deepansh Sagar <sagardeepansh96@gmail.com>
2025-05-16 19:16:34 +0530

    changes as per vercel

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const app = express();
 // Your existing middleware, routes, etc.
 // Example:
 const questionsRouter = require('../routes/questions');
-app.use('/questions', questionsRouter);
+app.use('/api', questionsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
