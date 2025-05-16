fd9060bc1633a48a73249c56683e226a7e406155
Deepansh Sagar <sagardeepansh96@gmail.com>
2025-05-16 19:23:33 +0530

    changes as per vercel

--- api/index.js
+++ api/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const questionRoutes = require('./routes/questions');
+const cors = require('cors');
 
-// Your existing middleware, routes, etc.
-// Example:
-const questionsRouter = require('../routes/questions');
-app.use('/api', questionsRouter);
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://deepanshsagar:UMzd6JOu0XR5nHlF@cluster0.cs8pize.mongodb.net/truthanddare?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+app.use('/api', questionRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
 
-module.exports = app;
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
